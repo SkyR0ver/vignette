a00bdf423dc9ff64c2066333c290df2e5254d4a0
SkyR0ver <anstonsolar@outlook.com>
2026-08-29 17:20:59 +0800

    Add convenient wrapper for device reader

--- vignette-core/src/hid.rs
+++ vignette-core/src/hid.rs
@@ -1,10 +1,25 @@
+use async_hid::AsyncHidWrite;
 use futures::StreamExt;
 
 use crate::error::{HidError, HidResult};
 
 type HidDevice = async_hid::Device;
 pub type HidDevInfo = async_hid::DeviceInfo;
-pub type HidDevReaderWriter = async_hid::DeviceReaderWriter;
+
+pub struct HidDevWriter(async_hid::DeviceWriter);
+
+impl HidDevWriter {
+    pub async fn write_output_report(&mut self, report_id: u8, data: &[u8]) -> HidResult<()> {
+        let mut buf = Vec::with_capacity(data.len() + 1);
+        buf.push(report_id);
+        buf.extend_from_slice(data);
+        self.0.write_output_report(&buf).await?;
+        Ok(())
+    }
+}
+
+pub type HidDevReader = async_hid::DeviceReader;
+pub type HidDevReaderWriter = (HidDevReader, HidDevWriter);
 
 pub(crate) async fn get_all() -> HidResult<Vec<HidDevInfo>> {
     let out = async_hid::HidBackend::default()
@@ -22,5 +37,6 @@ pub(crate) async fn open(devinfo: &HidDevInfo) -> HidResult<HidDevReaderWriter> 
         .await?
         .next()
         .ok_or(HidError::NotConnected)?;
-    Ok(device.open().await?)
+    let (reader, writer) = device.open().await?;
+    Ok((reader, HidDevWriter(writer)))
 }
