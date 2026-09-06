9f8c271577e4942c7e8a59953fb5e01270fa4a1a
SkyR0ver <anstonsolar@outlook.com>
2026-09-06 23:32:00 +0800

    Simplify ProtoError with thiserror

--- vignette-core/src/error.rs
+++ vignette-core/src/error.rs
@@ -1,40 +1,18 @@
-use std::fmt::Display;
+use thiserror::Error;
 
 pub use async_hid::{HidError, HidResult};
 pub use binrw::BinResult;
-pub type BinError = binrw::Error;
 
 pub type ProtoResult<T> = Result<T, ProtoError>;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum ProtoError {
-    HidError(HidError),
+    #[error("{0}")]
+    HidError(#[from] async_hid::HidError),
+    #[error("Invalid report ID: {0}")]
     InvalidReportId(u8),
-    BadFrame(BinError),
+    #[error("{0}")]
+    BadFrame(#[from] binrw::Error),
+    #[error("Response mismatch")]
     ResponseMismatch,
-}
-
-impl Display for ProtoError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ProtoError::HidError(e) => Display::fmt(e, f),
-            ProtoError::InvalidReportId(id) => write!(f, "Invalid report ID: {}", id),
-            ProtoError::BadFrame(e) => Display::fmt(e, f),
-            ProtoError::ResponseMismatch => write!(f, "Response mismatch"),
-        }
-    }
-}
-
-impl std::error::Error for ProtoError {}
-
-impl From<HidError> for ProtoError {
-    fn from(value: HidError) -> Self {
-        ProtoError::HidError(value)
-    }
-}
-
-impl From<BinError> for ProtoError {
-    fn from(value: BinError) -> Self {
-        ProtoError::BadFrame(value)
-    }
 }
