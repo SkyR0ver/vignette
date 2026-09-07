cdf382e511b50c7c14b1c1b459d1aec8d5b0e133
SkyR0ver <anstonsolar@outlook.com>
2026-09-07 09:59:43 +0800

    Add global protocal mapping

--- vignette-core/src/model.rs
+++ vignette-core/src/model.rs
@@ -1,5 +1,8 @@
+use std::{collections::HashMap, sync::OnceLock};
+
 use crate::hid::HidDevInfo;
 
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 struct DevSpec {
     vendor_id: u16,
     product_id: u16,
@@ -7,22 +10,47 @@ struct DevSpec {
     usage_id: u16,
 }
 
-impl PartialEq<HidDevInfo> for DevSpec {
-    fn eq(&self, other: &HidDevInfo) -> bool {
-        self.vendor_id == other.vendor_id
-            && self.product_id == other.product_id
-            && self.usage_page == other.usage_page
-            && self.usage_id == other.usage_id
+impl From<&HidDevInfo> for DevSpec {
+    fn from(dev: &HidDevInfo) -> Self {
+        DevSpec {
+            vendor_id: dev.vendor_id,
+            product_id: dev.product_id,
+            usage_page: dev.usage_page,
+            usage_id: dev.usage_id,
+        }
     }
 }
 
-const SUPPORTED_DEVICES: &[DevSpec] = &[DevSpec {
-    vendor_id: 0x320F,
-    product_id: 0x5055,
-    usage_page: 0xFF1C,
-    usage_id: 0x92,
-}];
+enum ProtoName {
+    Weisheng,
+}
+
+fn protocol_map() -> &'static HashMap<DevSpec, ProtoName> {
+    static PROTOCOL_MAP: OnceLock<HashMap<DevSpec, ProtoName>> = OnceLock::new();
+    PROTOCOL_MAP.get_or_init(|| {
+        HashMap::from([
+            (
+                DevSpec {
+                    vendor_id: 0x320F,
+                    product_id: 0x5055,
+                    usage_page: 0xFF1C,
+                    usage_id: 0x92,
+                },
+                ProtoName::Weisheng,
+            ),
+            (
+                DevSpec {
+                    vendor_id: 0x320F,
+                    product_id: 0x5088,
+                    usage_page: 0xFF1C,
+                    usage_id: 0x92,
+                },
+                ProtoName::Weisheng,
+            ),
+        ])
+    })
+}
 
 pub fn is_supported(dev: &HidDevInfo) -> bool {
-    SUPPORTED_DEVICES.iter().any(|spec| spec == dev)
+    protocol_map().contains_key(&dev.into())
 }
